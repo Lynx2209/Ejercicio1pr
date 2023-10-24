1169b7c215413e4fd9a13f937dd2dd0f7a3cbd77
Lynx2209 <csi23-jconazo@colegioaltair.net>
2023-10-24 12:11:18 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,28 @@
+﻿using Ejercicio1pr.Servicios;
+
+namespace Ejercicio1pr.Controladores
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int n1;
+            MenuInterfaz mi = new MenuImplementacion();
+            n1 = mi.pedirNum();
+            if (n1%2==0)
+            {
+                Console.WriteLine("El número " + n1 + " es par.");
+            }
+            else
+            {
+                Console.WriteLine("El número " + n1 + " es impar.");
+            }
+        }
+
+
+    }
+}
+
+
+
---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1pr.Servicios
+{
+    internal class MenuImplementacion:MenuInterfaz
+    {
+        public int pedirNum()
+        {
+            Console.WriteLine("Introduzca un numero: ");
+            int n1;
+            n1=Convert.ToInt32(Console.ReadLine());
+            return n1;
+        }
+    }
+}
